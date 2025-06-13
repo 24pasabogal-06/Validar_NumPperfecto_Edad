ed46f58ab9ceb6e5b373e82fee171bda5c00add6
24pasabogal-06 <pasabogalg@udistrital.edu.co>
2025-06-13 13:22:58 -0500

    Hola me ves?

---
+++ src/com/example/library/forms/BookEditor.java
@@ -0,0 +1,11 @@
+package com.example.library.forms;
+import javax.swing.*;
+private JPanel contentPane;
+
+    public class BookEditor extends JFrame {
+        private JPanel contentPane;
+        private JTextField bookNameField;
+        private JTextField authorNameField;
+        private JComboBox genreComboBox;
+        private JCheckBox isTakenCheckBox;
+    }
